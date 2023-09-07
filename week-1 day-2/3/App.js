0824af373b3b1098b1a364d250664b1a0ad7c1e5
bRylL318 <101691115+bRylL318@users.noreply.github.com>
2023-09-07 19:14:27 +0800

    Update App.js

--- week-1 day-2/3/App.js
+++ week-1 day-2/3/App.js
@@ -1,75 +1,48 @@
 import React, { Component } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import Addinfo from './Addinfo';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 class App extends Component {
   state = {
-    userData: null,
-    isSubmitted: false, // Track whether the form is submitted
-    activeTab: 0, // Track the active tab (0 for Home, 1 for About Us, 2 for Contact)
+    formData: null, // To store the user input data
   };
 
-  handleUserDataUpdate = (userData) => {
-    this.setState({ userData, isSubmitted: true }); // Set isSubmitted to true when the form is submitted
-  };
-
-  handleTabChange = (event, newValue) => {
-    this.setState({ activeTab: newValue });
+  handleUpdate = (data) => {
+    this.setState({ formData: data });
   };
 
   render() {
-    const { isSubmitted, userData, activeTab } = this.state;
-
     return (
       <div>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6">My App</Typography>
-            <Tabs value={activeTab} onChange={this.handleTabChange}>
-              <Tab label="Home" />
-              <Tab label="About Us" />
-              <Tab label="Contact" />
-            </Tabs>
+            <span style={{ flex: 1 }}>
+              {/* Add your Navbar items here */}
+              <span style={{ marginRight: '20px', cursor: 'pointer' }}>Home</span>
+              <span style={{ marginRight: '20px', cursor: 'pointer' }}>About Us</span>
+              <span style={{ marginRight: '20px', cursor: 'pointer' }}>Contact</span>
+            </span>
           </Toolbar>
         </AppBar>
-        <Container maxWidth="sm">
-          {activeTab === 0 && (
-            <div>
-              <h2>Welcome to the Home Page</h2>
-             
-            </div>
-          )}
-          {activeTab === 1 && (
-            <div>
-              <h2>About Us</h2>
-            </div>
-          )}
-          {activeTab === 2 && (
-            <div>
-              <h2>Contact Us</h2>
-            </div>
-          )}
-          <Addinfo onUpdate={this.handleUserDataUpdate} />
-          {isSubmitted && userData && (
-            <Card style={{ marginTop: '20px' }}>
+        <div style={{ padding: '20px' }}>
+          <h1>My React App</h1>
+          <Addinfo onUpdate={this.handleUpdate} />
+          {this.state.formData && (
+            <Card variant="outlined" style={{ marginTop: '20px' }}>
               <CardContent>
-                <h2>User Information:</h2>
-                <div>Name: {userData.name}</div>
-                <div>Date of Birth: {userData.dob}</div>
-                <div>Email: {userData.email}</div>
-                <div>Contact Number: {userData.contact}</div>
-                <div>About Me: {userData.about}</div>
+                <h2>User Information</h2>
+                <p>Name: {this.state.formData.name}</p>
+                <p>Date of Birth: {this.state.formData.dob}</p>
+                <p>Email ID: {this.state.formData.email}</p>
+                <p>Contact Number: {this.state.formData.contact}</p>
+                <p>About Me: {this.state.formData.about}</p>
               </CardContent>
             </Card>
           )}
-        </Container>
+        </div>
       </div>
     );
   }
