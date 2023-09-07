fa6e48ae330e775c1ae590f8fca26b8135fcb1e4
bRylL318 <101691115+bRylL318@users.noreply.github.com>
2023-09-07 19:44:32 +0800

    Update App.js

--- week-1 day-2/3/App.js
+++ week-1 day-2/3/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import Addinfo from './Addinfo';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import AddInfo from './Addinfo';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 
 class App extends Component {
   state = {
-    formData: null, 
+    formData: null, // To store the user input data
   };
 
   handleUpdate = (data) => {
@@ -20,7 +18,6 @@ class App extends Component {
         <AppBar position="static">
           <Toolbar>
             <span style={{ flex: 1 }}>
-           
               <span style={{ marginRight: '20px', cursor: 'pointer' }}>Home</span>
               <span style={{ marginRight: '20px', cursor: 'pointer' }}>About Us</span>
               <span style={{ marginRight: '20px', cursor: 'pointer' }}>Contact</span>
@@ -29,18 +26,28 @@ class App extends Component {
         </AppBar>
         <div style={{ padding: '20px' }}>
           <h1>My React App</h1>
-          <Addinfo onUpdate={this.handleUpdate} />
+          <AddInfo onUpdate={this.handleUpdate} />
           {this.state.formData && (
-            <Card variant="outlined" style={{ marginTop: '20px' }}>
-              <CardContent>
-                <h2>User Information</h2>
-                <p>Name: {this.state.formData.name}</p>
-                <p>Date of Birth: {this.state.formData.dob}</p>
-                <p>Email ID: {this.state.formData.email}</p>
-                <p>Contact Number: {this.state.formData.contact}</p>
-                <p>About Me: {this.state.formData.about}</p>
-              </CardContent>
-            </Card>
+            <div
+              className="card"
+              style={{ width: '18rem', marginTop: '20px', border: '1px solid #ccc' }}
+            >
+              <div className="card-body">
+                <h5 className="card-title">User Information</h5>
+                <p className="card-text">
+                  <strong>Name:</strong> {this.state.formData.name}
+                  <br />
+                  <strong>Date of Birth:</strong> {this.state.formData.dob}
+                  <br />
+                  <strong>Email ID:</strong> {this.state.formData.email}
+                  <br />
+                  <strong>Contact Number:</strong> {this.state.formData.contact}
+                  <br />
+                  <strong>About Me:</strong> {this.state.formData.about}
+                </p>
+
+              </div>
+            </div>
           )}
         </div>
       </div>
