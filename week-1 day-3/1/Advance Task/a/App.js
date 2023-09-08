0f34d1a5e161d92f1cb1ea791d969c24e167a0a8
bRylL318 <101691115+bRylL318@users.noreply.github.com>
2023-09-08 19:30:04 +0800

    Update App.js

--- week-1 day-3/1/Advance Task/a/App.js
+++ week-1 day-3/1/Advance Task/a/App.js
@@ -1,23 +1,39 @@
-const _ = require('lodash');
+import React from 'react';
+import _ from 'lodash';
 
-const arrOne = [{
+const arrOne = [
+  {
     id: 20,
     name: 'alex'
-}, {
+  },
+  {
     id: 30,
     name: 'alina'
-}];
+  }
+];
 
-const arrTwo = [{
+const arrTwo = [
+  {
     id: 40,
     name: 'hello'
-}, {
+  },
+  {
     id: 30,
     name: 'world'
-}];
+  }
+];
 
 const comparator = (obj1, obj2) => obj1.id === obj2.id;
 
 const result = _.xorWith(arrOne, arrTwo, comparator);
 
-console.log(result);
+function App() {
+  return (
+    <div>
+      <h1>Result:</h1>
+      <pre>{JSON.stringify(result, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default App;
